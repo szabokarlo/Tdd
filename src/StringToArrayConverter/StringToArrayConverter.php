5c90eff6f66eeb7b7939719f64cdaef10a97ad0c
szabok <karoly.szabo@PC173.luxoffice.duodecadits.com>
2015-03-12 16:59:30 +0100

    commit test

--- src/StringToArrayConverter/StringToArrayConverter.php
+++ src/StringToArrayConverter/StringToArrayConverter.php
@@ -4,11 +4,26 @@ namespace Tdd\StringToArrayConverter;
 
 class StringToArrayConverter
 {
+	/**
+	 * The input of the converter
+	 *
+	 * @var string
+	 */
 	private $input;
 
 	public function setInput($input)
 	{
-		 $this->input = $input;
+		if(!$this->isInputValid($input))
+		{
+			throw new \InvalidArgumentException('The input is invalid');
+		}
+
+		$this->input = $input;
+	}
+
+	public function isInputValid($input)
+	{
+		return is_string($input);
 	}
 
 	public function getInput()
